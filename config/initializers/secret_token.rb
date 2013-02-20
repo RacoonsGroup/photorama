92477540555fe9469fa28f4decb83dc23c3598ba
Andrey Nenashev <proudhamyak@gmail.com>
2013-02-20 13:54:44 +0400

    PhotoRama

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoRama::Application.config.secret_token = 'ee6c6bc7cb84329f35141fcb351dd93e2ac20b9cfc5da24868daeb84b15fac2bc936463dcd9b97f398e6056cc445a2ddfef25aee705a711f59de96ee57790c2c'
