7a0b29e6310a989e3aa7de0ece98032e44f81003
Andrey Nenashev <proudhamyak@gmail.com>
2013-02-20 18:39:37 +0400

    validations for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ActiveRecord::Base
   attr_accessible :subdomain, :title
   belongs_to :user
+
+  validates :title, :subdomain, presence: true
+  validates :title, :subdomain, length: { minimum: 2 }
+  validates :subdomain, uniqueness: true
 end
