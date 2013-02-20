b3df1c6e3bdfabd59e8d4990dd9ef5dbbdd86bb8
Marat Abdulin <esend.work@gmail.com>
2013-02-20 16:54:16 +0400

    reconfigure routes to subdomains

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,7 +1,7 @@
 module UrlHelper
-  def with_slug(slug)
-    slug = (slug || "")
-    slug += "." unless slug.empty?
-    [slug, request.domain, request.port_string].join
+  def with_subdomain(subdomain)
+    subdomain = (subdomain || "")
+    subdomain += "." unless subdomain.empty?
+    [subdomain, request.domain, request.port_string].join
   end
 end
