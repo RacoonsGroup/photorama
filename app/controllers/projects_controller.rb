71955f813c2b419b08b5aaad9aac2ce3a54b954f
Marat Abdulin <esend.work@gmail.com>
2013-02-20 17:14:50 +0400

    add show action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,8 @@
 class ProjectsController < ApplicationController
+  def show
+    if params[:slug].blank?
+      redirect_to root_url
+    end
+    @project = Project.find(params[:slug])
+  end
 end
