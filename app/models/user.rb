18a763c53fb7431358b2a05b1642b1bb48785204
Marat Abdulin <esend.work@gmail.com>
2013-02-20 16:10:38 +0400

    add association

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :city, :telephone, :full_name
   # attr_accessible :title, :body
+  has_one :project
 end
