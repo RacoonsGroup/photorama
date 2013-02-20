4ccecdbb6369ba70052f354278413e6f5b58049c
Marat Abdulin <esend.work@gmail.com>
2013-02-20 18:28:00 +0400

    fix url helper

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -2,6 +2,6 @@ module UrlHelper
   def with_subdomain(subdomain)
     subdomain = (subdomain || "")
     subdomain += "." unless subdomain.empty?
-    [subdomain, request.domain, request.port_string].join
+    [subdomain, request.domain].join
   end
 end
