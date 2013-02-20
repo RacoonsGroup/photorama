39a3bba3e391ed2c1295fc851f8fc8f07264e569
Andrey Nenashev <proudhamyak@gmail.com>
2013-02-20 17:53:29 +0400

    created project after registration

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,19 @@
 class ProjectsController < ApplicationController
+
+  before_filter :authenticate_user!, except: :show
+
   def show
-    if params[:slug].blank?
+    if params[:subdomain].blank?
       redirect_to root_url
     end
-    @project = Project.find(params[:slug])
+    @project = Project.find(params[:subdomain])
+  end
+
+  def new
+  end
+
+  def create
+    @project = Project.new(params[:project])
+    @project.save
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 PhotoRama::Application.routes.draw do
   devise_for :users
+  resources :projects
 
   root to: 'welcome#index'
   constraints(Subdomain) do
