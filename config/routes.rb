53cff9a31a5b6d94b1741b8b500f2665405f7ac0
Marat Abdulin <esend.work@gmail.com>
2013-02-20 16:40:21 +0400

    commented slug route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ PhotoRama::Application.routes.draw do
   devise_for :users
 
   root to: 'welcome#index'
-  constraints(Slug) do
-    match '/' => 'projects#show'
-  end
+  # constraints(Slug) do
+  #   match '/' => 'projects#show'
+  # end
 end
