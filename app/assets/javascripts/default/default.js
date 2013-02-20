45e80bc34ea1524d0ddae52cc598dc81a1941423
Marat Abdulin <esend.work@gmail.com>
2013-02-20 19:39:36 +0400

    add default layout for subdomains

---
+++ app/assets/javascripts/default/default.js
@@ -0,0 +1,29 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require default/application
+//= require default/bootstrap-alert
+//= require default/bootstrap-button
+//= require default/bootstrap-carousel
+//= require default/bootstrap-collapse
+//= require default/bootstrap-dropdown
+//= require default/bootstrap-modal
+//= require default/bootstrap-popover
+//= require default/bootstrap-scrollspy
+//= require default/bootstrap-tab
+//= require default/bootstrap-tooltip
+//= require default/bootstrap-transition
+//= require default/bootstrap-typeahead
+//= require default/bootstrap.min
+//= require default/jquery-ui-1.8.18.custom.min
+//= require default/jquery.smooth-scroll.min
+//= require default/lightbox
