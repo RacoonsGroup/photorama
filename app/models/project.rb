b917106d97375e25103093e2630f53b9d1a21bad
cy6eria <e.n.gundorov@gmail.com>
2013-02-21 13:10:42 +0400

    Fix charset

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
+# encoding=utf-8
 class Project < ActiveRecord::Base
   attr_accessible :subdomain, :title
   belongs_to :user
@@ -6,5 +7,5 @@ class Project < ActiveRecord::Base
   validates :title, :subdomain, length: { minimum: 2 }
   validates :subdomain, uniqueness: true
   validates :subdomain, format: { with: /^[a-z0-9][a-z0-9\-]*[a-z0-9]$/,
-  			 message: "в названии субдомена не должно быть спец.символов" }
+                                  message: "в названии субдомена не должно быть спец.символов" }
 end
