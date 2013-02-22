c5c80d9f137fc0e14c5893f3b894e071b7bdd52c
Marat Abdulin <esend.work@gmail.com>
2013-02-22 10:47:54 +0400

    fix after login redirect for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   include UrlHelper
   protect_from_forgery
   def after_sign_in_path_for(resource)
-    new_project_path
+    if current_path == new_admin_user_session
+      admin_root
+    else
+      new_project_path
+    end
   end
 
   private
