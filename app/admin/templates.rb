efac1f36cc26bd8f15667896382613b4605721fc
Marat Abdulin <esend.work@gmail.com>
2013-02-22 12:36:08 +0400

    create forms from admin, upload template images

--- app/admin/templates.rb
+++ app/admin/templates.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register Template do
-  
+  form do |f|
+    f.inputs :Templates do
+      f.input :name
+      f.input :layout_name
+      f.input :thumbnail
+    end
+    f.buttons
+  end
+
+  show do |f|
+    attributes_table do
+      row :name
+      row :layout_name
+      row :thumbnail do
+          image_tag(f.thumbnail.thumb.url)
+      end
+    end
+    active_admin_comments
+  end
 end
