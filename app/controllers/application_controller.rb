eed2948c73973417b8598847b4f26c1c6f16c266
Marat Abdulin <esend.work@gmail.com>
2013-02-22 11:27:28 +0400

    fix redirecting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include UrlHelper
   protect_from_forgery
   def after_sign_in_path_for(resource)
-    if current_path == new_admin_user_session
+    if url_for(:only_path=>true,:overwrite_params=>{}) == new_admin_user_session_path
       admin_root
     else
       new_project_path
