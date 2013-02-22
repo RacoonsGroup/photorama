4b2a93cb87433efb0496f28f8bdbd87e5a4bc1cf
Marat Abdulin <esend.work@gmail.com>
2013-02-22 14:43:49 +0400

    add gray_red to seed'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-  Template.create([{name: 'Default', layout_name: 'default'}, {name: 'Red', layout_name: 'red'}])
+  Template.create([{name: 'Default', layout_name: 'default'}, {name: 'Red', layout_name: 'red'}, {name: 'GrayRed', layout_name: 'gray_red'}])
