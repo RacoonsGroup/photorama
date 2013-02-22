120b6cdf0453e38481b4f923dac964e2b17a8806
Marat Abdulin <esend.work@gmail.com>
2013-02-22 10:55:29 +0400

    create seeds for Templates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+  Template.create([{name: 'Default', layout_name: 'default'}, {name: 'Red', layout_name: 'red'}])
