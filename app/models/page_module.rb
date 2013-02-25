fcc83cd4efb3af587f177b56745005b4c492e48e
Marat Abdulin <esend.work@gmail.com>
2013-02-25 16:46:18 +0400

    add array to types, and attr_accesible project_id

--- app/models/page_module.rb
+++ app/models/page_module.rb
@@ -1,4 +1,7 @@
+# encoding=utf-8
 class PageModule < ActiveRecord::Base
   belongs_to :project
-  attr_accessible :anchor, :slug, :type
+  attr_accessible :anchor, :slug, :type, :project_id
+
+  TYPES = [['Статическая страница', 'StaticPage']]
 end
