45e535be88da2115adfdf2e0c9c903b5d537eb60
Marat Abdulin <esend.work@gmail.com>
2013-02-26 15:59:19 +0400

    add validates to page_module

--- app/models/page_module.rb
+++ app/models/page_module.rb
@@ -3,5 +3,14 @@ class PageModule < ActiveRecord::Base
   belongs_to :project
   attr_accessible :anchor, :slug, :type, :project_id
 
+  validates :slug, format: { with: /^[a-z0-9][a-z0-9\-]*[a-z0-9]$/,
+                                  message: "в slug страницы не должно быть спец.символов" }
+  validates :slug,
+            presence: true,
+            uniqueness: { scope: :project_id }
+
+  validates :anchor,
+            presence: true
+
   TYPES = [['Статическая страница', 'StaticPage']]
 end
