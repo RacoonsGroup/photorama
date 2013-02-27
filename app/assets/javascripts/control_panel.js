ce16677266cb6c32ed0985a054ad3acbfcdf530f
cy6eria <e.n.gundorov@gmail.com>
2013-02-27 13:11:46 +0400

    Carousel for templates

--- app/assets/javascripts/control_panel.js
+++ app/assets/javascripts/control_panel.js
@@ -1,3 +1,4 @@
 //= require control_panel/control_panel
 //= require plugins/jquery.dd.min
+//= require plugins/jcarousellite_1.0.1.min
 //= require redactor-rails
--- app/assets/javascripts/control_panel/control_panel.js
+++ app/assets/javascripts/control_panel/control_panel.js
@@ -12,15 +12,17 @@ jQuery(function($) {
     var $templateOptions = $('#project_template_id option')
     var $selectedTemplateOption = $('#project_template_id option:selected')
 
-    $templateOptions.parent().hide().parents('div').append('<ul id="templates_list"></ul>')
+    $templateOptions.parent().hide().parents('#design').append('<button class="prev">Влево</button><button class="next">Вправо</button><div><ul id="templates_list"></ul></div>')
     $templateOptions.each(function(index, item) {
-        var listItem = '<li data-value="'+$(item).val()+'">'+'<img src="'+$(item).data('image')+'">'+$(item).text()+'</li>'
+        var listItem = '<li data-value="'+$(item).val()+'">'+'<img width="160px" height="100px" src="'+$(item).data('image')+'"></li>'//'+$(item).text()+'
         if ($(item).val() == $selectedTemplateOption.val()) {
             $('#templates_list').append(listItem).find('li:last').addClass('active')
         } else {
             $('#templates_list').append(listItem)
         }
     })
+
+    $('#templates_list').parent().jCarouselLite({visible:2,btnNext: ".next",btnPrev: ".prev"})
 
     $('#templates_list li').on('click', function() {
         $('#templates_list li').removeClass('active')
