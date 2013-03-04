59a279347d3760e661264b97e7fc5c8aa0c95971
Snuff <ggg1321@gmail.com>
2013-03-04 16:01:58 +0400

    Funcy box helper and partial

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,7 +1,7 @@
 module ProjectsHelper
 
   def link_to_photo(photo)
-    link_to image_tag( photo.file.thumb.url, alt: photo.title, class: 'img-polaroid', rel: 'group1'),  photo.file.url, class: 'photos'
+    link_to image_tag( photo.file.thumb.url, alt: photo.title, class: 'img-polaroid'),  photo.file.url, class: 'photos', rel: 'group1'
   end
 
 end
