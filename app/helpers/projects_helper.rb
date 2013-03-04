288a1c152c1b424a45e7230b730efeb75c1c6870
Snuff <ggg1321@gmail.com>
2013-03-04 17:56:45 +0400

    Add yield for head

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,6 +1,6 @@
 module ProjectsHelper
 
-  def link_to_photo(photo)
+  def link_to_fancy_photo(photo)
     link_to image_tag( photo.file.thumb.url, alt: photo.title, class: 'img-polaroid'),  photo.file.url, class: 'photos', rel: 'group1'
   end
 
