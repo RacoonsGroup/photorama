7d001d1db174ae326adcda4d8a7bdd4eff670f8c
Snuff <ggg1321@gmail.com>
2013-03-04 19:38:18 +0400

    Partial for gallery with galleriffic

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -4,4 +4,8 @@ module ProjectsHelper
     link_to image_tag( photo.file.thumb.url, alt: photo.title, class: 'img-polaroid'),  photo.file.url, class: 'photos', rel: 'group1'
   end
 
+  def link_to_galleriffic_photo(photo)
+    link_to image_tag( photo.file.thumb.url, alt: photo.title, class: 'span2'),  photo.file.url, class: 'thumb', name: photo.id, title: photo.title
+  end
+
 end
