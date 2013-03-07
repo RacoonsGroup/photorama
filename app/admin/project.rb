54aa563d9900c22a69c5b30aa5568e82e3e82f42
Marat Abdulin <esend.work@gmail.com>
2013-03-07 18:36:11 +0400

    logo uploader added

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -8,6 +8,7 @@ ActiveAdmin.register Project do
       f.input :template_id, as: :select, :collection => Template.all
       f.input :color_scheme_id, as: :select, :collection => ColorScheme.all
       f.input :background_id, as: :select, :collection => Background.all
+      f.input :logo, as: :file
     end
     f.buttons
   end
