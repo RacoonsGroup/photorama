59308a44772974ed995efb9149cb118999234d28
Marat Abdulin <esend.work@gmail.com>
2013-03-07 16:27:50 +0400

    routes changed for new action menu update order

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ PhotoRama::Application.routes.draw do
       resources :page_modules
 
 
-      post 'page_modules/menu_update' => 'page_modules#menu_update', as: :menu_update
+      post 'page_modules/menu_update_order' => 'page_modules#menu_update_order', as: :menu_update
       post 'page_modules/delete_page' => 'page_modules#delete_page', as: :delete_page
       post 'page_modules/retrieve_page' => 'page_modules#retrieve_page', as: :retrieve_page
       post 'page_modules/update_page' => 'page_modules#update_page', as: :update_page
